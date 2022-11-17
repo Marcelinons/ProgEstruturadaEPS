8ed26b6e7563837ef3307bbc7dccaa960816ffcd
Nicolas Marcelino da Silva <76798076+Marcelinons@users.noreply.github.com>
2022-11-17 11:51:48 -0300

    Added new methods to DynamicList.c

--- Week08/DynamicList.c
+++ Week08/DynamicList.c
@@ -12,10 +12,10 @@ List* CreateList() {
 
 
 void FreeMemory(List *li) {
- 
+
     if (li != NULL) {
         Elem *no = NULL;
-        /* Runs through the list until the content of *li (a memory address) is 
+        /* Runs through the list until the content of *li (a memory address) is
         NULL (until it finds the end of the list).
         */
         while ((*li) != NULL) {
@@ -52,4 +52,137 @@ int EmptyList(List *li) {
             return 0;
         }
     }
+}
+
+int InserNewElemBeginning(List *li, int n) {
+    if (li == NULL) {
+        return 0;
+    } else {
+        Elem *node = NULL;
+        node = (Elem *) malloc(sizeof(Elem));
+        if (node == NULL) {
+            return 0;
+        } else {
+            node->num = n;
+            node->next = *li;
+            *li = node;
+        }
+    }
+}
+
+int InsertNewElem(List *li, int n) {
+    // Checks if its possible to insert a new element.
+    if (li == NULL) {
+        return 0;
+    }
+    Elem *node = NULL;
+    node = (Elem *) malloc(sizeof(Elem));
+    if (node == NULL) {
+        return 0;
+    }
+    node->num = n;
+    node->next = NULL;
+    // If the list is empty, the last element is also the first.
+    if (*li == NULL) {
+        *li = node;
+    } else {
+        // Runs through the list searching for its last element.
+        Elem *aux;
+        aux = *li;
+        while (aux->next != NULL) {
+            aux = aux->next;
+        }
+        // When it finds the last element of the list, set its pointer to the
+        // newly created element.
+        aux->next = node;
+    }
+    return 1;
+}
+
+int GetElemInPos(List *li, int pos) {
+    if (li == NULL || pos < 0) {
+        exit(0);
+    }
+    Elem *node = NULL;
+    node = *li;
+
+    int i = 1, resp = 0;
+    while (node != NULL && i < pos) {
+        node = node->next;
+        i++;
+    }
+    if (node == NULL) {
+        exit(0);
+    } else {
+        resp = node->num;
+        return resp;
+    }
+}
+
+float MeanFFive(List *li) {
+    int sum = 0, i = 0;
+    if (li == NULL) {
+        exit(0);
+    }
+    for (i = 0; i < 5; i++) {
+        sum += GetElemInPos(li, i+1);
+    }
+    return sum/(float)5;
+}
+
+void Head(List *li) {
+    int i = 0;
+    if (li == NULL) {
+        exit(0);
+    }
+    for (i = 0; i < 5; i++) {
+        printf("%d ", GetElemInPos(li, i+1));
+    }
+    printf("\n");
+}
+
+int CountPairElems(List *li) {
+    int i, count = 0, elem;
+    if (li == NULL) {
+        exit(0);
+    }
+    for (i = 0; i < 5; i++) {
+        elem = GetElemInPos(li, i+1);
+        if (elem%2 == 0) {
+            count++;
+        }
+    }
+    return count;
+}
+
+int LargestAmongFFive(List *li, int *largPos) {
+    int i, largest, elem;
+    if (li == NULL) {
+        exit(0);
+    }
+    for (i = 0; i < 5; i++) {
+        elem = GetElemInPos(li, i+1);
+        if (i == 0) {
+            largest = elem;
+        } else {
+            if (elem > largest) {
+                largest = elem;
+                *largPos = i+1;
+            }
+        }
+    }
+    return largest;
+}
+
+int SumFFive(List *li) {
+    int i, sum = 0, elem;
+    if (li == NULL) {
+        exit(0);
+    } else {
+        for (i = 0; i < 5; i++) {
+            elem = GetElemInPos(li, i+1);
+            sum += elem;
+        }
+    }
+    return sum;
 }
